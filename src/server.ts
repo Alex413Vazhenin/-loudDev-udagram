3299313c72165571f69dc6502d47c487fa644dc6
Alexander Vazhenin <a.vazhenin413@gmail.com>
2020-12-11 17:40:17 +0500

    Updated build procedure for windows OS

--- src/server.ts
+++ src/server.ts
@@ -13,23 +13,24 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get("/filteredimage/", async (req, res) => {
+    let image_url = req.query.image_url;
 
-  /**************************************************************************** */
+    const imageValidation = String(image_url).includes('.png')  || String(image_url).includes('.jpg') ||
+                           String(image_url).includes('.jpeg') && !String(image_url).includes('http');
 
-  //! END @TODO1
+    if(!imageValidation) {
+      res.status(400).send('Image URL is missing or not valid');
+    }
+
+    const filteredImage = await filterImageFromURL(image_url);
+    res.status(200).sendFile(filteredImage, err =>{
+      if(err){
+        res.status(404).send(err)
+      }
+      deleteLocalFiles([filteredImage]);
+    });
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
